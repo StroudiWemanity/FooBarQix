bfec927e39b4414826d8a04308623cb967b7041f
Sofien Troudi <stroudi@wedigital.garden>
2019-08-30 14:43:53 +0200

    should return a Foo each time when the entry contains 3 : test OK

--- src/app/services/contain-check/contain-check.service.ts
+++ src/app/services/contain-check/contain-check.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ContainCheckService {
-
-  constructor() { }
+  public turn3IntoFoos(entry: number): string {
+    let contentString = '';
+    entry.toString().split('').map((char: string) => {
+      if (char === '3') {
+        contentString += 'Foo';
+      }
+    });
+    return contentString;
+  }
 }
