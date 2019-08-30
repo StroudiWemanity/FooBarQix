d5a53c9425245b8ab70d82ae743569d2ca3b6413
Sofien Troudi <stroudi@wedigital.garden>
2019-08-30 14:24:41 +0200

    should respect the first set of kata rules when the entry is correct : global test KO

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,3 +16,35 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 });
+
+describe('compute function', () => {
+  it(`should compute '3' into 'FooFoo'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.compute(3)).toContain('FooFoo');
+  });
+
+  it(`should compute '7' into 'QixQix'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.compute(7)).toContain('QixQix');
+  });
+
+  it(`should compute '15' into 'FooBarBar'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.compute(15)).toContain('FooBarBar');
+  });
+
+  it(`should compute '51' into 'FooBar'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.compute(51)).toContain('FooBar');
+  });
+
+  it(`should not compute '8' into something else`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.compute(8)).toContain('8');
+  });
+});
