73f2a7be8b16775efaa737bfcb8a3cd13dabbf66
Sofien Troudi <stroudi@wedigital.garden>
2019-08-30 14:45:07 +0200

    should return a Bar each time when the entry contains 5 : test KO

--- src/app/services/contain-check/contain-check.service.spec.ts
+++ src/app/services/contain-check/contain-check.service.spec.ts
@@ -17,4 +17,12 @@ describe('ContainCheckService', () => {
     expect(service.turn3IntoFoos(33)).toBe('FooFoo');
     expect(service.turn3IntoFoos(6)).not.toBe('Foo');
   });
+
+  it(`should return 'Foo' for each 3 entry contains`, () => {
+    const service: ContainCheckService = TestBed.get(ContainCheckService);
+    expect(service.turn5IntoBars(5)).toBe('Bar');
+    expect(service.turn5IntoBars(15)).toBe('Bar');
+    expect(service.turn5IntoBars(55)).toBe('BarBar');
+    expect(service.turn5IntoBars(10)).not.toBe('Bar');
+  });
 });
