c5d4329ff3b29ea9c9ba22e5fa4b6618ece3a6c6
Sofien Troudi <stroudi@wedigital.garden>
2019-08-30 14:31:15 +0200

    should return Bar when a 5 is entered : test OK

--- src/app/services/division-check/division-check.service.ts
+++ src/app/services/division-check/division-check.service.ts
@@ -7,4 +7,8 @@ export class DivisionCheckService {
   public turn3IntoFoo(entry: number): string {
     return entry % 3 === 0 ? 'Foo' : '';
   }
+
+  public turn5IntoBar(entry: number): string {
+    return entry % 5 === 0 ? 'Bar' : '';
+  }
 }
