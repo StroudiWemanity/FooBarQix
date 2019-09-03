5b5211e95ba3effb76febb904af1d26d67d1b056
Sofien Troudi <stroudi@wedigital.garden>
2019-09-03 15:25:30 +0200

    should return * each time when the entry contains 0 : test OK

--- src/app/services/contain-check/contain-check.service.ts
+++ src/app/services/contain-check/contain-check.service.ts
@@ -16,13 +16,18 @@ export class ContainCheckService {
     return entry === '7' ? 'Qix' : '';
   }
 
+  private static turn0IntoStar(entry: string): string {
+    return entry === '0' ? '*' : '';
+  }
+
   public turnAll(entry: number): string {
     let contentString = '';
     entry.toString().split('').map((char: string) => {
       contentString +=
         ContainCheckService.turn3IntoFoo(char) +
         ContainCheckService.turn5IntoBar(char) +
-        ContainCheckService.turn7IntoQix(char);
+        ContainCheckService.turn7IntoQix(char) +
+        ContainCheckService.turn0IntoStar(char);
     });
     return contentString;
   }
